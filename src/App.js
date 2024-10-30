b9a9b4a24e68a7c2f2a4caa5b81b7120f5ed6f62
QuangHuy164 <nghuy164@gmail.com>
2024-10-30 04:32:43 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-route-dom';
 import { Box } from '@mui/material'
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
-        <Box width='400px'>
-            Navbar 
+        <Box width='400px' sx={{width: { xl: '1488px'}}} m="auto">
+            <Navbar /> 
             <Routes>
                 <Route path='/' element={<Home />}>
-                
+                <Route path='/exercise/:id' element= {<ExerciseDetail />} />
                 </Route>
             </Routes>
+            <Footer />
             </Box>
     )
 }
